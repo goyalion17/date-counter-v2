99546dddffeb5c89981d07566d786c4c9aa6d9a0
Irina Saveleva <goyalion17@gmail.com>
2024-01-02 15:31:04 +0100

    controlled date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("januar 02 2024");
+  date.setDate(date.getDate() + counter);
+
+  function handleReset() {
+    setCounter(0);
+    setStep(1);
+  }
+
+  return (
+    <div className="counterContainer">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
+      <div>
+        <button onClick={() => setCounter((s) => s - step)}>-</button>
+        <input
+          type="text"
+          value={counter}
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
+        <button onClick={() => setCounter((s) => s + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {counter === 0
+            ? "Today is "
+            : counter > 0
+            ? `${counter} days from today is `
+            : `${Math.abs(counter)} days ago was `}
+        </span>
+        {date.toDateString()}
+      </p>
+      {counter !== 0 || step !== 1 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
+    </div>
+  );
+}
